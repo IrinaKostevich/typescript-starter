25e256d35a2f01d700e29f59a3abebeb50279d85
Irina Kostevich <irina.kostevich@outlook.com>
2021-08-28 15:23:34 +1000

    ESLint config update

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-restricted-syntax': 'off'
+  }
+};
